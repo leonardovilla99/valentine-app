d581593ce3ab5d26c90c1385eef21f07abc6200f
Leonardo Villa <villaleonardo99@gmail.com>
2024-02-05 09:55:37 -0500

    init

---
+++ src/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React, { useState } from "react";
+import "./App.css";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 10 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "Really sure?",
+      "Think again!",
+      "Last chance!",
+      "Surely not?",
+      "You might regret this!",
+      "Give it another thought!",
+      "Are you absolutely certain?",
+      "This could be a mistake!",
+      "Have a heart!",
+      "Don't be so cold!",
+      "Change of heart?",
+      "Wouldn't you reconsider?",
+      "Is that your final answer?",
+      "You're breaking my heart ;(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="container">
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/eiMqmu0vXz8AAAAi/kiss.gif" alt="" />
+          <div className="textBody">Ok yay!!!</div>
+        </>
+      ) : (
+        <>
+          <img
+            className="image"
+            src="https://media.tenor.com/PxwDtGv_ROkAAAAi/cute-love.gif"
+            alt=""
+          />
+          <h1 className="textTitle">Will you be my Valentine?</h1>
+          <div>
+            <button
+              className="yesButton"
+              style={{
+                fontSize: yesButtonSize,
+                width: yesButtonSize * 3,
+                height: yesButtonSize * 2,
+              }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button onClick={handleNoClick} className="noButton">
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
